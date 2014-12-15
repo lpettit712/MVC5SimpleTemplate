06b489abfa7379587f4c8837f37280a6da9b4e93
lpettit712 <lpettit712@gmail.com>
2014-12-15 07:08:02 -0800

    Added deployment creation functionality.
    Added deployment creation functionality.

--- Site/Controllers/DeploymentController.cs
+++ Site/Controllers/DeploymentController.cs
@@ -1,20 +1,36 @@
 ﻿using System.Web.Mvc;
 using SiteServices.ViewModelProviders;
+using Contract;
+using Web.Core;
 
 namespace Site.Controllers
 {
     public class DeploymentController : Controller
     {
         private readonly IDeploymentViewModelProvider _deploymentViewModelProvider;
+        private readonly ILoggingService _loggingService;
 
-        public DeploymentController(IDeploymentViewModelProvider deploymentViewModelProvider)
+        public DeploymentController(IDeploymentViewModelProvider deploymentViewModelProvider, ILoggingService loggingService)
         {
             _deploymentViewModelProvider = deploymentViewModelProvider;
+            _loggingService = loggingService;
         }
 
         public ActionResult Index()
         {
             return View(_deploymentViewModelProvider.GetDeploymentsViewModel());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Create(Contract.Models.Deployment deployment)
+        {
+            _loggingService.Info("Deployment is being created.");
+            return RedirectToAction("Index");
         }
     }
 }
